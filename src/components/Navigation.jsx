df4264abb912e3db91085c74efb42ba3b7148484
Abdul Khaliq <ak882710@gmail.com>
2024-11-09 01:43:29 +0530

    added css

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className="App">
+    <nav className='container'>
+      <div className='logo'>
+      <img src="/images/brand_logo.png" alt=""/>
+      </div>
+      <ul>
+       <li href="#">Menu</li>
+       <li href="#">Location</li>
+       <li href="#">About</li>
+      </ul>
+      <button>login</button>
+    </nav>
+         
+   </div>
+  )
+}
+
+export default Navigation
